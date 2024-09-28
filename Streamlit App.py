65d468812a0198a8288d14d612efe669d51aa8d6
Nkortega21 <nkortega21@gmail.com>
2024-09-28 11:45:24 -0700

    committing requirements

--- Streamlit App.py
+++ Streamlit App.py
@@ -10,10 +10,3 @@ df = pd.DataFrame({
 })
 df
 
-import streamlit as st
-import pandas as pd
- df = pd.DataFrame({
- 'first column': [1, 2, 3, 4],
- 'second column': [10, 20, 30, 40]
-})
- st.line_chart(df,x='first column', y='second column')
